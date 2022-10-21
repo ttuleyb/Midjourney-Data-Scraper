2d4c2481c6aa9eece3fca6d738443a67b09df613
Tuleyb <74624982+ttuleyb@users.noreply.github.com>
2022-10-21 14:44:10 +0100

    Delete python sql to csv.py

--- MJ Miner/python sql to csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sqlite3
-
-db_file = "aesthetic.sqlite"
-
-conn = sqlite3.connect(db_file, isolation_level=None,
-                       detect_types=sqlite3.PARSE_COLNAMES)
-db_df = pd.read_sql_query("SELECT * FROM generations", conn)
-db_df.to_csv('database.csv', index=False)
