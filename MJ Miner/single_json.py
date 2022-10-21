1a584984ddfee8fddf2e5669eb2a376525a0350d
Tuleyb <74624982+ttuleyb@users.noreply.github.com>
2022-10-21 14:44:40 +0100

    Delete single_json.py

--- MJ Miner/single_json.py
+++
@@ -1,8 +0,0 @@
-from json import load
-
-everything = []
-with open("jsons\\" + "1.json", "r", encoding="utf-8") as inputFile:
-    data = load(inputFile)
-    for i in data:
-        print(i["full_command"])
-        everything.append(i["prompt"])
